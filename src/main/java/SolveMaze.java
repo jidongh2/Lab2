0fa1e75cca45327f41057690538a8f4888889898
Jidong Huanng <jidongh2@illinois.edu>
2018-01-31 14:50:36 -0600

    mm

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,12 +35,27 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
-        }
+            while (maze.isFinished() == false) {
+                if (maze.canMove() == false) {
+                    maze.turnRight();
+                    if (maze.canMove() == false) {
+                        maze.turnRight();
+                        if (maze.canMove() == false) {
+                            maze.turnRight();
+                        }
+                    }
+                }
+                if (maze.canMove() == true) {
+                    maze.move();
+                    maze.turnLeft();
+                }
 
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
+                if (maze.isFinished()) {
+                    System.out.println("You solved the maze!");
+                } else {
+                    System.out.println("Try again!");
+                }
+            }
         }
     }
 }
